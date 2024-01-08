f39eedebfd5d98036b7812eaef588fce9eb1de66
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-08 12:10:00 +0100

    Task 1 - Tested locally : OK

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_insert_left - creates a binary tree node
+ *@parent: pointer to the node to insert the left-child in
+ *@value: the value to store in the new node
+ * Return: Pointer to the new node // NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+		/*If left is NULL, create a new node and assign it to left child*/
+		parent->left = binary_tree_node(parent, value);
+
+	else
+		return (NULL);
+
+	return (parent->left);
+}
