7f78b65684fe12ab944ab532e35c5a7ba1becc12
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-08 15:43:57 +0100

    Task 7 - Tested locally : OK Added proto to .h file

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_inorder - goes through a binary tree using in-order traversal
+ *@tree: pointer to the root node of the tree to traverse
+ *@func: pointer to a function to call for each node.
+ * Return: void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	return;
+
+	/* Traverse the left subtree in-order */
+	binary_tree_inorder(tree->left, func);
+	/* Apply the function to the current node */
+	func(tree->n);
+	/* Traverse the right subtree in-order */
+	binary_tree_inorder(tree->right, func);
+}
