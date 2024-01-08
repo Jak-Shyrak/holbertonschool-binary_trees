a11347191b0e1c9344b48acbe46a08ba675be7e4
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-08 14:46:19 +0100

    Added proto function of task 3 to .h // Added proper documentation and comments to task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,21 +2,18 @@
 
 /**
  *binary_tree_delete - delete a node
- *@parent: pointer to the node to insert the right-child in
- *@value: the value to store in the new node
- * Return: Pointer to the new node // NULL on failure or if parent is NULL
+ *@tree: a pointer to the root node of the tree to delete
+ * Return: void
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return;
-	}
 
-	// First delete the left and right subtrees
+	/*First delete the left and right subtrees*/
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 
-	// Then delete the current node
+	/*Then delete the current node*/
 	free(tree);
 }
