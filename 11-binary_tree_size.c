e35bb473934937f2ea9ae02f20868c0b7f9e0bc5
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-09 07:17:19 +0100

    Change comment for betty requirement of 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - Measures the depth of a node in a binary tree.
- * @tree: A pointer to the node to measure the depth.
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree.
  *
- * Return: The depth of the node. If `tree` is NULL, return 0.
+ * Return: The size of the tree. If `tree` is NULL, return 0.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
