0998a132c64cdb9bce117f1856151f480af40b8d
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-09 07:22:25 +0100

    Change comment for betty requirement of 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - Measures the size of a binary tree.
- * @tree: A pointer to the root node of the tree.
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the leaves.
  *
- * Return: The size of the tree. If `tree` is NULL, return 0.
+ * Return: The number of leaves in the tree. If `tree` is NULL, return 0.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
